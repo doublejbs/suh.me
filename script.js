b71c44519e63cf2b6a40905db2486471afe63aaa
doublejbs <doublejbs@naver.com>
2018-08-26 20:23:29 +0900

    수정

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+$(document).ready(function(){
+    $('.menu-title').click(function(){
+        console.log('u');
+        
+        if($(this).next().css('display') == 'none'){
+            console.log('none');
+            $(this).next().css('display', 'block'); 
+        }
+        else{
+            $(this).next().css('display', 'none');
+        }
+        
+
+    });
+    
+    $('.logo').click(function(){
+       console.log('hi'); 
+    });
+});
